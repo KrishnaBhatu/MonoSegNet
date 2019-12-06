9f6df1594e60930a46da5e686e16def59d435530
KrishnaBhatu <42816789+KrishnaBhatu@users.noreply.github.com>
2019-12-06 05:05:28 -0500

    Set default path of Data to nothing(empty string)

--- monodepth_main.py
+++ monodepth_main.py
@@ -30,7 +30,7 @@ parser.add_argument('--mode',                      type=str,   help='train or te
 parser.add_argument('--model_name',                type=str,   help='model name', default='monodepth')
 parser.add_argument('--encoder',                   type=str,   help='type of encoder, vgg or resnet50', default='vgg')
 parser.add_argument('--dataset',                   type=str,   help='dataset to train on, kitti, or cityscapes', default='kitti')
-parser.add_argument('--data_path',                 type=str,   help='path to the data', required=True)
+parser.add_argument('--data_path',                 type=str,   help='path to the data', default="")
 parser.add_argument('--filenames_file',            type=str,   help='path to the filenames text file', required=True)
 parser.add_argument('--input_height',              type=int,   help='input height', default=256)
 parser.add_argument('--input_width',               type=int,   help='input width', default=512)
