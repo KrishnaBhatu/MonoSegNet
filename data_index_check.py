6b3f861eb0e0409f1afffdb5d0393e6f64e90d28
KrishnaBhatu <42816789+KrishnaBhatu@users.noreply.github.com>
2019-12-07 02:55:06 -0500

    Remove commit later

--- data_index_check.py
+++ data_index_check.py
@@ -5,6 +5,7 @@ f= open("/home/adi_leo96_av/training_index.txt", 'r')
 lines = f.readlines()
 for line in lines:
   new_line = line.rsplit('\n')
+  print(new_line)
   for subline in new_line.split():
     if(not os.path.exists(subline)):
       print(subline)
