388f730875302bfd3541b74eb8700ffe8aff154e
KrishnaBhatu <42816789+KrishnaBhatu@users.noreply.github.com>
2019-12-07 02:40:31 -0500

    Adding new file for data verification

---
+++ data_index_check.py
@@ -0,0 +1,12 @@
+import os
+
+f= open("/home/adi_leo96_av/training_index.txt", 'r')
+
+lines = f.readlines()
+for line in lines:
+  new_line = line.rsplit('\n')
+  for subline in new_line.split():
+    if(!os.path.exists(subline)):
+      print(subline)
+  
+
