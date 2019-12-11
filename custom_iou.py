b0ede46548d633b14430c1c6405048de5f83de06
hrishikeshtawade04 <hrishikeshtawade04@gmail.com>
2019-12-10 22:29:25 -0500

    Modified the custom IOU for test dataset

--- custom_iou.py
+++ custom_iou.py
@@ -3,20 +3,6 @@ import numpy as np
 import cv2
 import glob
 
-input_images = []
-for folder_name in glob.glob('C:/Users/hrish/Desktop/Testing/cityscapesScripts-master/MonoSegNet/sample_data/*'):
-    for input_image_path in glob.glob(folder_name + "/*.png"):
-        print(input_image_path)
-        a = cv2.imread(input_image_path)
-        print(a.shape)
-
-eval_segm = cv2.imread('C:/Users/hrish/Desktop/Testing/cityscapesScripts-master/MonoSegNet/sample_data/augsburg/pred.png')
-eval_segm = eval_segm/5
-gt_segm = cv2.imread('C:/Users/hrish/Desktop/Testing/cityscapesScripts-master/MonoSegNet/sample_data/augsburg/gt.png')
-#gt_segm = cv2.resize(gt_segm, (512 , 256), interpolation = cv2.INTER_NEAREST)
-eval_segm = eval_segm[:,:,0]
-gt_segm = gt_segm[:,:,0]
-
 def pixel_accuracy(eval_segm, gt_segm):
     '''
     sum_i(n_ii) / sum_i(t_i)
@@ -44,36 +30,31 @@ def pixel_accuracy(eval_segm, gt_segm):
         return pixel_accuracy_
     return -1
 
-def mean_IU(eval_segm, gt_segm):
-    '''
-    (1/n_cl) * sum_i(n_ii / (t_i + sum_j(n_ji) - n_ii))
-    '''
+def find_IU(eval_segm, gt_segm,IU,IU_occurences):
 
     check = check_size(eval_segm, gt_segm)
     
-    if check:
-        cl, n_cl   = union_classes(eval_segm, gt_segm)
-        _, n_cl_gt = extract_classes(gt_segm) # calc number of classes in gt
-        eval_mask, gt_mask = extract_both_masks(eval_segm, gt_segm, cl, n_cl) # number of masks will be more for each eval and gt both
+    assert check == True
 
-        IU = list([0]) * n_cl # making a list to store all the IOUs
+    cl, n_cl   = union_classes(eval_segm, gt_segm)
+    _, n_cl_gt = extract_classes(gt_segm) # calc number of classes in gt
+    eval_mask, gt_mask = extract_both_masks(eval_segm, gt_segm, cl, n_cl) # number of masks will be more for each eval and gt both
 
-        for i, c in enumerate(cl):
-            curr_eval_mask = eval_mask[i, :, :]
-            curr_gt_mask = gt_mask[i, :, :]
-    
-            if (np.sum(curr_eval_mask) == 0) or (np.sum(curr_gt_mask) == 0):
-                continue
+    for i, c in enumerate(cl):
+        curr_eval_mask = eval_mask[i, :, :]
+        curr_gt_mask = gt_mask[i, :, :]
 
-            n_ii = np.sum(np.logical_and(curr_eval_mask, curr_gt_mask)) # intersection
-            t_i  = np.sum(curr_gt_mask) # 
-            n_ij = np.sum(curr_eval_mask) #union
+        if (np.sum(curr_eval_mask) == 0) or (np.sum(curr_gt_mask) == 0):
+            continue
 
-            IU[i] = n_ii / (t_i + n_ij - n_ii) # preventing the double counting of intersection
-    
-        mean_IU_ = np.sum(IU) / n_cl_gt #classes in gt but not predicted by model are considered in acc but not vice versa
-        return mean_IU_ # penalize FP and FN
-    return -1
+        n_ii = np.sum(np.logical_and(curr_eval_mask, curr_gt_mask)) # intersection
+        t_i  = np.sum(curr_gt_mask) # 
+        n_ij = np.sum(curr_eval_mask) #union
+
+        IU[c] += n_ii / (t_i + n_ij - n_ii) # preventing the double counting of intersection
+        IU_occurences[c] += 1
+    #mean_IU_ = np.sum(IU) / n_cl_gt #classes in gt but not predicted by model are considered in acc but not vice versa
+    return (IU, IU_occurences) # penalize FP and FN
 
 def union_classes(eval_segm, gt_segm):
     eval_cl, _ = extract_classes(eval_segm)
@@ -123,12 +104,27 @@ def extract_masks(segm, cl, n_cl):
     return masks
 
 def test():
-    cl,n_cl = extract_classes(eval_segm)
-    print("eval clases = {} and number = {}".format(cl,n_cl))
-    cl,n_cl = extract_classes(gt_segm)
-    print("Gt clases = {} and number = {}".format(cl,n_cl))
-    check_size(eval_segm, gt_segm)
-    print("Pixel Accuracy = ", pixel_accuracy(eval_segm, gt_segm))
-    print("Mean IOU = ", mean_IU(eval_segm, gt_segm))
+    #cl,n_cl = extract_classes(eval_segm)
+    #print("eval clases = {} and number = {}".format(cl,n_cl))
+    #cl,n_cl = extract_classes(gt_segm)
+    #print("Gt clases = {} and number = {}".format(cl,n_cl))
+    #check_size(eval_segm, gt_segm)
+    #print("Pixel Accuracy = ", pixel_accuracy(eval_segm, gt_segm))
+    IU = list([0]) * 35 # making a list to store all the IOUs
+    IU_occurences = list([0])*35 # for the 35 clasees    
+    with open('/home/adi_leo96_av/MonoSegNet/*.txt','r') as f:
+        for line in f:
+            paths = line.split(' ')
+            eval_segm = cv2.imread(path[0])
+            gt_segm = cv2.imread(path[1])
+            gt_segm = cv2.resize(gt_segm,(512,256),interpolation=cv2.INTER_NEAREST)
+            (IU,IU_occurences) = find_IU(eval_segm[:,:,0], gt_segm[:,:,0],IU,IU_occurences)
+    sum = 0
+    for i in range(34):
+        IU[i] = IU[i]/IU_occurences[i] 
+        sum += IU[i]
+
+    print("IOU = ", IU)
+    print("mIOU = ", sum/35)
     
 #test()
